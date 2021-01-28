b600f1d0e00aa840c66e1f4ba10496d254cc5f74
Ian Cook <ianmcook@gmail.com>
2021-01-28 16:27:03 -0500

    Fix parameterized Spark versions

--- tasks/write_all_types/caller.py
+++ tasks/write_all_types/caller.py
@@ -1,12 +1,12 @@
 from polyspark import run_on_spark
 from pkg_resources import parse_version
 
-#vers = ['2.0.0', '3.0.0'] # assigned in YAML
 comps_200 = ['none', 'snappy', 'gzip'] #, 'lzo'] # need extra libraries
 comps_240 = [] #'brotli', 'lz4', 'zstd'] # need extra libraries
 
-for ver in vers:
-  comps = comps_200
-  if parse_version(ver) >= parse_version('2.4.0'):
-    comps += comps_240
-  run_on_spark('tasks/write_all_types/script.py', ver, compression = comps)
+def run(vers=['2.0.0', '3.0.0']):
+    for ver in vers:
+      comps = comps_200
+      if parse_version(ver) >= parse_version('2.4.0'):
+        comps += comps_240
+      run_on_spark('tasks/write_all_types/script.py', ver, compression = comps)
